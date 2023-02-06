bf1a1b60dee0e41286e2e93d48238865dfc7be50
kantusablade <124055780+kantusablade@users.noreply.github.com>
2023-02-06 03:08:11 +0300

    Add files via upload

---
+++ 1zadanie.cs
@@ -0,0 +1,63 @@
+using System;
+
+class Phone
+{
+    private string numberModel;
+    private double weight;
+    private string phoneNumber;
+
+    public Phone(string numberModel, double weight, string phoneNumber)
+    {
+        this.numberModel = numberModel;
+        this.weight = weight;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Phone() : this("Unknown", 0.0, "Unknown") { }
+
+    public void receiveCall(string name)
+    {
+        Console.WriteLine("Звонит " + name);
+    }
+
+    public void receiveCall(string name, string number)
+    {
+        Console.WriteLine("Звонит " + name + " Номер: " + number);
+    }
+
+    public string getNumber()
+    {
+        return phoneNumber;
+    }
+
+    public void SendMessage(params string[] numbers)
+    {
+        Console.Write("Сообщение отправлено на номера: ");
+        foreach (string number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Phone phone1 = new Phone("Model 1", 0.5, "111");
+        Phone phone2 = new Phone("Model 2", 0.7, "222");
+        Phone phone3 = new Phone("Model 3", 0.9, "333");
+
+        phone1.receiveCall("yara");
+        Console.WriteLine("Number: " + phone1.getNumber());
+
+        phone2.receiveCall("yana", "222");
+        Console.WriteLine("Number: " + phone2.getNumber());
+
+        phone3.receiveCall("zxcchudila");
+        Console.WriteLine("Number: " + phone3.getNumber());
+
+        phone1.SendMessage("444", "555", "666");
+    }
+}
